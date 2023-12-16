5404beac21d6572876ef0e358804a4e00e33668e
Ruslan <ru.abdullin@innopolis.university>
2023-12-16 23:07:35 +0300

    lab 4

--- 04_arithmetics_and_pipelining/04_03_06_sqrt_formula_pipe/04_05_shift_register_with_valid.sv
+++ 04_arithmetics_and_pipelining/04_03_06_sqrt_formula_pipe/04_05_shift_register_with_valid.sv
@@ -53,34 +53,41 @@ endmodule
 // Task 5
 //----------------------------------------------------------------------------
 
-module shift_register_with_valid #(
-    parameter WIDTH = 8,
-    parameter DEPTH = 8
-) (
+module shift_register_with_valid
+# (
+    parameter width = 8, depth = 8
+)
+(
     input                clk,
     input                rst,
-    input                in_vld,
-    input  [WIDTH - 1:0] in_data,
-    output               out_vld,
-    output [WIDTH - 1:0] out_data
-);
 
-    logic [WIDTH - 1:0] data [DEPTH-1:0];
-    logic [DEPTH-1:0]   vld;
+    input                in_vld,
+    input  [width - 1:0] in_data,
+
+    output               out_vld,
+    output [width - 1:0] out_data
+);
+    logic [width - 1:0] data [0:depth - 1];
+    logic [depth - 1:0] vld;
 
     always_ff @ (posedge clk) begin
         if (rst) begin
-            data <= '{default: '0};
-            vld <= '{default: 1'b0};
-        end else begin
-            if (in_vld) begin
-                data <= {data[DEPTH-2:0], in_data};
-                vld <= {vld[DEPTH-2:0], 1'b1};
+            for (int i = 0; i < depth; i++) begin
+                data[i] <= '0;
+                vld[i] <= 1'b0;
             end
+        end else begin
+            for (int i = depth - 1; i > 0; i--) begin
+                data[i] <= data[i-1];
+                vld[i] <= vld[i-1];
+            end
+
+            data[0] <= in_vld ? in_data : data[0];
+            vld[0] <= in_vld;
         end
     end
 
-    assign out_data = data[DEPTH - 1];
-    assign out_vld = vld[DEPTH - 1];
+    assign out_data = data[depth - 1];
+    assign out_vld = vld[depth - 1];
 
 endmodule
--- 04_arithmetics_and_pipelining/04_03_06_sqrt_formula_pipe/04_06_formula_2_pipe.sv
+++ 04_arithmetics_and_pipelining/04_03_06_sqrt_formula_pipe/04_06_formula_2_pipe.sv
@@ -1,3 +1,46 @@
+module fifo #(parameter WIDTH = 32, DEPTH = 16)
+(
+    input clk,
+    input rst,
+    input [WIDTH-1:0] data_in,
+    input vld_in,
+    output reg [WIDTH-1:0] data_out,
+    output reg vld_out
+);
+
+    reg [WIDTH-1:0] mem [0:DEPTH-1];
+    reg [$clog2(DEPTH):0] read_ptr, write_ptr;
+    wire fifo_full, fifo_empty;
+
+    assign fifo_empty = (read_ptr == write_ptr);
+    assign fifo_full = ((write_ptr + 1'b1) == read_ptr);
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            read_ptr <= 0;
+            write_ptr <= 0;
+            vld_out <= 1'b0;
+            data_out <= {WIDTH{1'b0}};
+        end else begin
+            if (vld_in && !fifo_full) begin
+                mem[write_ptr[($clog2(DEPTH)-1):0]] <= data_in;
+                write_ptr <= write_ptr + 1'b1;
+            end
+
+            if (!fifo_empty && (read_ptr != write_ptr)) begin
+                data_out <= mem[read_ptr[($clog2(DEPTH)-1):0]];
+                vld_out <= 1'b1;
+                read_ptr <= read_ptr + 1'b1;
+            end else begin
+                vld_out <= 1'b0;
+            end
+        end
+    end
+
+endmodule
+
+
+
 //----------------------------------------------------------------------------
 // Task 6
 //----------------------------------------------------------------------------
@@ -12,29 +55,93 @@ module formula_2_pipe
     input  [31:0] b,
     input  [31:0] c,
 
-    output        res_vld,
-    output [31:0] res
+    output reg        res_vld,
+    output reg [31:0] res
 );
-    // Task:
-    //
-    // Implement a pipelined module formula_2_pipe that computes the result
-    // of the formula defined in the file formula_2_fn.svh.
-    //
-    // The requirements:
-    //
-    // 1. The module formula_2_pipe has to be pipelined.
-    //
-    // It should be able to accept a new set of arguments a, b and c
-    // arriving at every clock cycle.
-    //
-    // It also should be able to produce a new result every clock cycle
-    // with a fixed latency after accepting the arguments.
-    //
-    // 2. Your solution should instantiate exactly 3 instances
-    // of a pipelined isqrt module, which computes the integer square root.
-    //
-    // 3. Your solution should save dynamic power by properly connecting
-    // the valid bits.
 
+    // Parameters for the pipeline stages
+    parameter N_STAGES = 16;
+    parameter FIFO_DEPTH_1 = N_STAGES;
+    parameter FIFO_DEPTH_2 = 2 * N_STAGES + 1;
+
+    // Intermediate valid signals and results
+    wire vld1, vld2, vld3;
+    wire [31:0] res1, res2, res3;
+
+    // Instantiate the first pipelined isqrt module
+    isqrt #(.n_pipe_stages(N_STAGES)) isqrt1
+    (
+        .clk(clk),
+        .rst(rst),
+        .x_vld(arg_vld),
+        .x(c),
+        .y_vld(vld1),
+        .y(res1)
+    );
+
+    // FIFOs to align the data for the next stage
+    wire [31:0] fifo1_data;
+    wire fifo1_vld;
+
+    fifo #(.DEPTH(FIFO_DEPTH_1)) fifo1
+    (
+        .clk(clk),
+        .rst(rst),
+        .data_in(b),
+        .vld_in(arg_vld),
+        .data_out(fifo1_data),
+        .vld_out(fifo1_vld)
+    );
+
+    // Instantiate the second pipelined isqrt module
+    isqrt #(.n_pipe_stages(N_STAGES)) isqrt2
+    (
+        .clk(clk),
+        .rst(rst),
+        .x_vld(fifo1_vld),
+        .x(fifo1_data + res1),
+        .y_vld(vld2),
+        .y(res2)
+    );
+
+    // FIFOs to align the data for the next stage
+    wire [31:0] fifo2_data;
+    wire fifo2_vld;
+
+    fifo #(.DEPTH(FIFO_DEPTH_2)) fifo2
+    (
+        .clk(clk),
+        .rst(rst),
+        .data_in(a),
+        .vld_in(arg_vld),
+        .data_out(fifo2_data),
+        .vld_out(fifo2_vld)
+    );
+
+    // Instantiate the third pipelined isqrt module
+    isqrt #(.n_pipe_stages(N_STAGES)) isqrt3
+    (
+        .clk(clk),
+        .rst(rst),
+        .x_vld(vld2),
+        .x(fifo2_data + res2),
+        .y_vld(vld3),
+        .y(res3)
+    );
+
+    // Register to hold the result
+    always @(posedge clk)
+    begin
+        if (rst)
+        begin
+            res <= 0;
+            res_vld <= 0;
+        end
+        else
+        begin
+            res <= res3;
+            res_vld <= vld3;
+        end
+    end
 
 endmodule
